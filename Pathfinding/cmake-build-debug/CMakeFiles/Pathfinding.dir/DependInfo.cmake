3c4a695699e5f77953ccabbff138f054545759e3
Carlos Eduardo Leahy Vásquez <leahyvasquez@gmail.com>
2018-10-29 20:16:28 -0600

    Inicialización del proyecto
    Se estuvo desarrollando el proyecto por aparte y hasta ahora se va a subir al repositorio

---
+++ Pathfinding/cmake-build-debug/CMakeFiles/Pathfinding.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/Algoritmo AStar/MapSearchNode.cpp" "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/cmake-build-debug/CMakeFiles/Pathfinding.dir/Algoritmo_AStar/MapSearchNode.cpp.o"
+  "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/Algoritmo AStar/execAStar.cpp" "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/cmake-build-debug/CMakeFiles/Pathfinding.dir/Algoritmo_AStar/execAStar.cpp.o"
+  "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/globalArray.cpp" "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/cmake-build-debug/CMakeFiles/Pathfinding.dir/globalArray.cpp.o"
+  "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/main.cpp" "/home/leahycarlos21/Documents/ITCR/II Semestre 2018/Algoritmos y Estructuras de Datos II/Proyectos CE/Proyecto II/A* Star/Pathfinding/cmake-build-debug/CMakeFiles/Pathfinding.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pathfinding/cmake-build-debug/CMakeFiles/Pathfinding.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pathfinding.dir/main.cpp.o"
+  "CMakeFiles/Pathfinding.dir/Algoritmo_AStar/MapSearchNode.cpp.o"
+  "CMakeFiles/Pathfinding.dir/Algoritmo_AStar/execAStar.cpp.o"
+  "CMakeFiles/Pathfinding.dir/globalArray.cpp.o"
+  "Pathfinding.pdb"
+  "Pathfinding"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pathfinding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
